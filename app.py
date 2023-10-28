daa79b9c35150e46c8cacffc2bff58e97ce3e82b
jens <jens.schunter@studmail.hs-aalen.de>
2023-10-28 17:19:11 +0200

    feat: add data product level 3

--- app.py
+++ app.py
@@ -41,6 +41,38 @@ def provide_derived_data(raw_data_df):
     return home_stats, away_stats
 
 
+def provide_algorithm(raw_data_df):
+    with st.expander("Algorithm for Home Advantage"):
+        st.markdown(
+            """
+                    **Heimvorteil sind +3 Punkte im Handicap**  
+                    *(Modellierte Annahme)*
+                    
+                    ---
+
+                    Erklärung:
+            
+                    Die Schätzung des Heimvorteils in der NFL auf ungefähr 2,5 bis 3 Punkte pro Spiel basiert auf einer Kombination von historischen Daten, Studien und Erfahrungen von Sportanalysten. Es ist wichtig zu beachten, dass dies eine allgemeine Schätzung ist und keine exakte wissenschaftliche Berechnung darstellt. Hier sind einige der Quellen und Grundlagen, auf denen diese Schätzung basiert:
+
+                    - **Historische Daten:** Durch die Analyse von jahrzehntelangen NFL-Spielprotokollen können Sportanalysten Muster erkennen, die darauf hinweisen, dass Teams, die zu Hause spielen, tendenziell bessere Ergebnisse erzielen als bei Auswärtsspielen. Dies kann als Ausgangspunkt für die Schätzung des Heimvorteils dienen.
+
+                    - **Akademische Studien:** Es gibt einige akademische Studien und wissenschaftliche Arbeiten, die den Heimvorteil im Sport, einschließlich der NFL, untersuchen. Diese Studien nutzen statistische Methoden, um den Heimvorteil zu quantifizieren. Obwohl die Ergebnisse variieren können, zeigen viele dieser Studien einen Heimvorteil von etwa 2,5 bis 3 Punkten pro Spiel.
+
+                    - **Erfahrung von Sportanalysten:** Sportexperten und Analysten, die die NFL und andere Sportligen abdecken, bringen ihre Erfahrung und Einsichten in die Schätzung des Heimvorteils ein. Dies kann auf beobachteten Mustern und ihrer Kenntnis der Dynamik von Heim- und Auswärtsspielen basieren.
+                    """
+        )
+
+        prepared_data_df = raw_data_df.copy()
+
+        condition_1 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] > 3
+        condition_2 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] < 0
+
+        prepared_data_df["true wins"] = condition_1 | condition_2
+
+        st.write(prepared_data_df)
+    return
+
+
 def main():
     st.title("NFL-Predictor")
 
@@ -51,6 +83,9 @@ def main():
 
     # Level 2
     home_stats, away_stats = provide_derived_data(raw_data_df=raw_data_df)
+
+    # Level 3
+    provide_algorithm(raw_data_df=raw_data_df)
 
     return
 
