9d94a744a5f8574186969a08587e2d375f02daea
jens <jens.schunter@studmail.hs-aalen.de>
2023-10-28 16:47:27 +0200

    chore: update data product level 1-2

--- app.py
+++ app.py
@@ -2,19 +2,26 @@ import json
 import streamlit as st
 import pandas as pd
 
-def provide_raw_data(data: dict) -> None:
+
+def provide_raw_data(path):
+    with open(file=path, mode="r") as raw_file:
+        raw_data = json.load(raw_file)
+
+    raw_data_df = pd.DataFrame(raw_data["games"])
+
+    home_team = st.selectbox(label="Home Team", options=raw_data["teams"], index=0)
+    away_team = st.selectbox(label="Away Team", options=raw_data["teams"], index=1)
 
     with st.expander(label="Raw Data"):
-        st.json(data)
+        st.json(raw_data)
 
-    return
+    return raw_data_df, home_team, away_team
 
 
-def provide_derived_data(df: pd.DataFrame) -> None:
+def provide_derived_data(raw_data_df):
     with st.expander(label="Insights"):
-
         st.subheader("Home Insights")
-        home_stats = df.groupby("team")[["points_scored", "points_allowed"]].mean()
+        home_stats = raw_data_df.groupby("team")[["points_scored", "points_allowed"]].mean()
         home_stats["team"] = home_stats.index
         home_stats.sort_values("points_scored", ascending=False, inplace=True)
         home_stats.reset_index(drop=True, inplace=True)
@@ -22,35 +29,31 @@ def provide_derived_data(df: pd.DataFrame) -> None:
         st.write(home_stats)
 
         st.subheader("Away Insights")
-        away_stats = df.groupby("opponent")[["points_scored", "points_allowed"]].mean()
+        away_stats = raw_data_df.groupby("opponent")[
+            ["points_scored", "points_allowed"]
+        ].mean()
         away_stats["team"] = away_stats.index
         away_stats.sort_values("points_scored", ascending=False, inplace=True)
         away_stats.reset_index(drop=True, inplace=True)
         away_stats.index += 1
         st.write(away_stats)
 
+    return home_stats, away_stats
 
-
-    return
 
 def main():
     st.title("NFL-Predictor")
 
-    with open(file="data/nfl_data.json", mode="r") as raw_file:
-        raw_data = json.load(raw_file)
-
-    raw_data_df = pd.DataFrame(raw_data["games"])
-    
-    st.selectbox(label="Home Team", options=raw_data["teams"], index=0)
-    st.selectbox(label="Away Team", options=raw_data["teams"], index=1)
+    path = "data/nfl_data.json"
 
     # Level 1
-    provide_raw_data(data=raw_data)
-    
-   # Level 2
-    provide_derived_data(df=raw_data_df)
-   
+    raw_data_df, home_team, away_team = provide_raw_data(path=path)
+
+    # Level 2
+    home_stats, away_stats = provide_derived_data(raw_data_df=raw_data_df)
+
     return
+
 
 if __name__ == "__main__":
     main()
