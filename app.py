dce9965eab4e45fde6f4debea0733fa9bffd5709
jens <jens.schunter@studmail.hs-aalen.de>
2023-12-03 15:52:44 +0100

    feat: add level 2 endpoints

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from io import StringIO
 import requests
 import streamlit as st
 import pandas as pd
@@ -6,6 +7,7 @@ import pandas as pd
 URL = "http://127.0.0.1:8000"
 ENDPOINT_DATA = URL+"/level-1/data"
 ENDPOINT_TEAMS = URL+"/level-1/teams"
+ENDPOINT_STATS = URL+"/level-2/stats"
 
 def provide_raw_data():
     response = requests.get(url=ENDPOINT_DATA)        
@@ -17,31 +19,24 @@ def provide_raw_data():
     return
 
 
-def provide_derived_data(raw_data_df):
-
-    raw_data_df = pd.DataFrame(raw_data["games"])
-
+def provide_derived_data():
     with st.expander(label="Insights"):
-        st.subheader("Home Insights")
-        home_stats = raw_data_df.groupby("team")[["points_scored", "points_allowed"]].mean()
-        home_stats["team"] = home_stats.index
-        home_stats.sort_values("points_scored", ascending=False, inplace=True)
-        home_stats.reset_index(drop=True, inplace=True)
-        home_stats.index += 1
-        st.write(home_stats)
+        team_types = ["team", "opponent"]
 
-        st.subheader("Away Insights")
-        away_stats = raw_data_df.groupby("opponent")[
-            ["points_scored", "points_allowed"]
-        ].mean()
-        away_stats["team"] = away_stats.index
-        away_stats.sort_values("points_scored", ascending=False, inplace=True)
-        away_stats.reset_index(drop=True, inplace=True)
-        away_stats.index += 1
-        st.write(away_stats)
+        for team_type in team_types:
+            if team_type == "team":
+                label = "Home"
+            else:
+                label = "Away"
 
-    return home_stats, away_stats
+            st.subheader(f"{label} Insights")
+            
+            response = requests.get(url=ENDPOINT_STATS, params={"team_type": team_type})        
+            raw_data = response.json()
+            df = pd.read_json(StringIO(raw_data), orient = "index")
+            st.write(df)
 
+    return
 
 def provide_algorithm(raw_data_df):
     with st.expander("Algorithm for Home Advantage"):
@@ -143,7 +138,7 @@ def main():
     provide_raw_data()
 
     # # Level 2
-    # home_stats, away_stats = provide_derived_data(raw_data_df=raw_data_df)
+    provide_derived_data()
 
     # # Level 3
     # provide_algorithm(raw_data_df=raw_data_df)
--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,34 @@
 import json
+import pandas as pd
 from fastapi import FastAPI
 
 PATH = "data/nfl_data.json"
 
 app= FastAPI()
 
-@app.get("/level-1/data")
-
-async def get_data():
+def FileHandler():
     with open(file=PATH, mode="r") as raw_file:
         data = json.load(raw_file)
     return data
 
-@app.get("/level-1/teams")
 
+@app.get("/level-1/data")
 async def get_data():
-    with open(file=PATH, mode="r") as raw_file:
-        data = json.load(raw_file)
+    return FileHandler()
+
+@app.get("/level-1/teams")
+async def get_data():
+    data = FileHandler()
     return data["teams"]
+
+@app.get("/level-2/stats")
+async def get_stats(team_type:str = "team"):
+    raw_data = FileHandler()
+    raw_data_df = pd.DataFrame(raw_data["games"])
+
+    stats = raw_data_df.groupby(team_type)[["points_scored", "points_allowed"]].mean()
+    stats[team_type] = stats.index
+    stats.sort_values("points_scored", ascending=False, inplace=True)
+    stats.reset_index(drop=True, inplace=True)
+    stats.index += 1  
+    return stats.to_json(orient = "index")
