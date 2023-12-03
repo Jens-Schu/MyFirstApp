5c3af92fd42758b05f1afabe3da0cb23dff38b7a
jens <jens.schunter@studmail.hs-aalen.de>
2023-12-03 18:25:34 +0100

    feat: Integrate Level 3 in Backend

---
+++ backend/main.py
@@ -0,0 +1,45 @@
+import json
+import pandas as pd
+from fastapi import FastAPI
+
+PATH = "data/nfl_data.json"
+
+app= FastAPI()
+
+def FileHandler():
+    with open(file=PATH, mode="r") as raw_file:
+        data = json.load(raw_file)
+    return data
+
+
+@app.get("/level-1/data")
+async def get_data():
+    return FileHandler()
+
+@app.get("/level-1/teams")
+async def get_data():
+    data = FileHandler()
+    return data["teams"]
+
+@app.get("/level-2/stats")
+async def get_stats(team_type:str = "team"):
+    raw_data = FileHandler()
+    raw_data_df = pd.DataFrame(raw_data["games"])
+
+    stats = raw_data_df.groupby(team_type)[["points_scored", "points_allowed"]].mean()
+    stats[team_type] = stats.index
+    stats.sort_values("points_scored", ascending=False, inplace=True)
+    stats.reset_index(drop=True, inplace=True)
+    stats.index += 1  
+    return stats.to_json(orient = "index")
+
+@app.get("/level-3/algorithm")
+async def algorithm():
+    raw_data = FileHandler()
+    raw_data_df = pd.DataFrame(raw_data["games"])
+
+    condition_1 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] > 3
+    condition_2 = raw_data_df["points_scored"] - raw_data_df["points_allowed"] < 0
+
+    raw_data_df["true wins"] = condition_1 | condition_2
+    return raw_data_df.to_json(orient = "index")
